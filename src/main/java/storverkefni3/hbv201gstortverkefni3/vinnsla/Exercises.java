c2d4d34d262331b9053a5ee23a0d211e766c84af
brynjar <brynjar23@live.com>
2023-03-27 15:02:16 +0000

    added comment to exercises about what an exercise needs

--- src/main/java/storverkefni3/hbv201gstortverkefni3/vinnsla/Exercises.java
+++ src/main/java/storverkefni3/hbv201gstortverkefni3/vinnsla/Exercises.java
@@ -3,4 +3,5 @@ package storverkefni3.hbv201gstortverkefni3.vinnsla;
 public class Exercises {
    // an exercise has a type like curls is a strength type and curls has reps and sets
     // but for cardio instead of reps it would have a time duration which would get added to a timer
+    //an exercise has a name and a gif
 }
