db4ec752e5c59f73fface8a0e5ee421440b3022d
brynjar <brynjar23@live.com>
2023-03-27 14:23:21 +0000

    renamed hellocontroller and added workoutcontroller

--- src/main/java/storverkefni3/hbv201gstortverkefni3/vidmot/InitialController.java
+++ src/main/java/storverkefni3/hbv201gstortverkefni3/vidmot/InitialController.java
@@ -3,7 +3,7 @@ package storverkefni3.hbv201gstortverkefni3.vidmot;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class InitialController {
     @FXML
     private Label welcomeText;
 
---
+++ src/main/java/storverkefni3/hbv201gstortverkefni3/vidmot/WorkoutController.java
@@ -0,0 +1,7 @@
+package storverkefni3.hbv201gstortverkefni3.vidmot;
+
+public class WorkoutController {
+    public static void main(String[] args) {
+
+    }
+}
