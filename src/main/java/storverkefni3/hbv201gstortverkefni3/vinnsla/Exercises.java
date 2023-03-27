4a69d4d77aa73311d75b487fe58c49ba59c7cd6b
brynjar <brynjar23@live.com>
2023-03-27 22:33:56 +0000

    Added getters and a toString method to Exercises and methods to get all Exercises in workouts for weightloss and musclegain

--- src/main/java/storverkefni3/hbv201gstortverkefni3/vinnsla/Exercises.java
+++ src/main/java/storverkefni3/hbv201gstortverkefni3/vinnsla/Exercises.java
@@ -5,6 +5,7 @@ import javafx.beans.property.*;
 
 
 public class Exercises {
+    // Brynjar Steinn worked on this class
    // an exercise has a type like curls is a strength type and curls has reps and sets
     // but for cardio instead of reps it would have a time duration which would get added to a timer
     //an exercise has a name and a gif
@@ -34,4 +35,16 @@ public class Exercises {
     private void setTime(double time) {
         this.time.set(time);
     }
-}
+    public String getName(){
+        return name.get();}
+    public Double getTime(){
+        return time.get();}
+    public int getReps(){return reps.get();}
+    public int getSets(){return sets.get();}
+    @Override
+    public String toString() {
+        return getName() + " " + getTime() + " " + getSets()+ " " + getReps();
+
+        }
+    }
+
--- src/main/java/storverkefni3/hbv201gstortverkefni3/vinnsla/Workouts.java
+++ src/main/java/storverkefni3/hbv201gstortverkefni3/vinnsla/Workouts.java
@@ -1,9 +1,22 @@
 package storverkefni3.hbv201gstortverkefni3.vinnsla;
 
+import javafx.collections.ObservableList;
+
 import java.util.ArrayList;
 
 public class Workouts {
-   ArrayList workout = new ArrayList<>();
+   //Brynjar Steinn worked on this class
+   protected ObservableList<Exercises> workout;
 
-   // if (time != 0){then add time?}
+   public ObservableList<Exercises> getAllMusclegainExercises() {
+      //exercise(name,time,sets,reps)
+workout.add(new Exercises("Curls",0.0,3,10 ));
+return workout;
+   }
+   public ObservableList<Exercises> getAllWeightlossExercises() {
+      //exercise(name,time,sets,reps)
+      workout.add(new Exercises("Jumping jacks",30.0,0,0 ));
+      return workout;
+   }
 }
+// if (time != 0){then add time?}
