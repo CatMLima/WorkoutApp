a49d1c79aa196f471199536ca09be7451a273304
Catarina <catisousalima1999@gmail.com>
2023-03-27 13:29:55 +0000

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module storverkefni3.hbv201gstortverkefni3 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens storverkefni3.hbv201gstortverkefni3 to javafx.fxml;
+    exports storverkefni3.hbv201gstortverkefni3;
+}
---
+++ src/main/java/storverkefni3/hbv201gstortverkefni3/HelloController.java
@@ -0,0 +1,14 @@
+package storverkefni3.hbv201gstortverkefni3;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
