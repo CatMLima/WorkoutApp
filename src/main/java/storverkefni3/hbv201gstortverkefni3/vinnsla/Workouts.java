48258cc695acd12ef7906e59cf6bde6430133726
brynjar <brynjar23@live.com>
2023-03-30 14:54:23 +0000

    removed weightloss list and changed musclegain to be workout

--- src/main/java/storverkefni3/hbv201gstortverkefni3/vinnsla/Workouts.java
+++ src/main/java/storverkefni3/hbv201gstortverkefni3/vinnsla/Workouts.java
@@ -7,24 +7,24 @@ import javafx.collections.ObservableList;
 public class Workouts {
    public Workouts(){
       workoutNames = FXCollections.observableArrayList();
-      musclegain = FXCollections.observableArrayList();
-      weightloss = FXCollections.observableArrayList();
+      workout = FXCollections.observableArrayList();
+
    }
    //Brynjar Steinn worked on this class
    protected ObservableList<WorkoutName> workoutNames;
-   protected ObservableList<Exercises> weightloss;
-protected ObservableList<Exercises> musclegain;
+
+protected ObservableList<Exercises> workout;
 
    public ObservableList<Exercises> getAllMusclegainExercises() {
       //exercise(name,time,sets,reps)
-musclegain.add(new Exercises("Muscle Gain","Curls",0.0,3,10 ));
-      musclegain.add(new Exercises("Muscle Gain","Squats",0.0,3,10 ));
-return musclegain;
+workout.add(new Exercises("Muscle Gain","Curls",0.0,3,10 ));
+      workout.add(new Exercises("Muscle Gain","Squats",0.0,3,10 ));
+return workout;
    }
    public ObservableList<Exercises> getAllWeightlossExercises() {
       //exercise(name,time,sets,reps)
-      weightloss.add(new Exercises("Weight loss","Jumping jacks",30.0,0,0 ));
-      return weightloss;
+      workout.add(new Exercises("Weight loss","Jumping jacks",30.0,0,0 ));
+      return workout;
    }
    public ObservableList<WorkoutName> getAllMusclegainWorkouts() {
       workoutNames.add(new WorkoutName("Arms"));
